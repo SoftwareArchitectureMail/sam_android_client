189dac8cb0340d6c4d7b93d048d9570f2087d622
Steven Bustos Beltran <jsbustosb@unal.edu.co>
2017-10-05 00:14:53 -0500

    AddFontAwesome and example with button icon

--- app/src/main/java/com/sam/teamd/samandroidclient/MainActivity.java
+++ app/src/main/java/com/sam/teamd/samandroidclient/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sam.teamd.samandroidclient;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import me.anwarshahriar.calligrapher.Calligrapher;
 
@@ -12,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Typeface fontAwesomeFont = Typeface.createFromAsset(getAssets(), "font/fontawesome-webfont.ttf");
+
         // To use the same font in all the APP
         Calligrapher calligrapher = new Calligrapher(this);
         calligrapher.setFont(this, "font/SourceSansPro-Regular.ttf", true);
+
+        Button fontAwesomeUniversityIcon = (Button) findViewById(R.id.font_awesome_university_icon);
+        fontAwesomeUniversityIcon.setTypeface(fontAwesomeFont);
     }
 }
