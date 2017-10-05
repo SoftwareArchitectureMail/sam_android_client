45abb1f8afc1431abaf4a3b09b265fb3796b73bf
Steven Bustos Beltran <jsbustosb@unal.edu.co>
2017-10-04 23:54:27 -0500

    Same font in all APP

--- app/src/main/java/com/sam/teamd/samandroidclient/MainActivity.java
+++ app/src/main/java/com/sam/teamd/samandroidclient/MainActivity.java
@@ -3,11 +3,17 @@ package com.sam.teamd.samandroidclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.anwarshahriar.calligrapher.Calligrapher;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // To use the same font in all the APP
+        Calligrapher calligrapher = new Calligrapher(this);
+        calligrapher.setFont(this, "font/SourceSansPro-Regular.ttf", true);
     }
 }
