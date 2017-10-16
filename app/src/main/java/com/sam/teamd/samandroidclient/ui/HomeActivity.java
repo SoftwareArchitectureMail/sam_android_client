5fcd3c4a6e7c2866d18ef695c9d44a85b01a20e6
Yessica <ykchautai@unal.edu.co>
2017-10-16 17:17:32 -0500

    Barra de búsqueda ok

--- app/src/main/java/com/sam/teamd/samandroidclient/ui/HomeActivity.java
+++ app/src/main/java/com/sam/teamd/samandroidclient/ui/HomeActivity.java
@@ -84,7 +84,7 @@ public class HomeActivity extends AppCompatActivity
             }
         });
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_home_settings, menu);
+            
         return true;
     }
 
