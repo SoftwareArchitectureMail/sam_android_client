a744302f2eb8efca437fd14023132763a56ee5e4
Steven Bustos Beltran <jsbustosb@unal.edu.co>
2017-10-16 22:12:37 -0500

    Vista de detalle de mensaje

---
+++ app/src/main/java/com/sam/teamd/samandroidclient/ui/ViewMailActivity.java
@@ -0,0 +1,15 @@
+package com.sam.teamd.samandroidclient.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sam.teamd.samandroidclient.R;
+
+public class ViewMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_mail);
+    }
+}
